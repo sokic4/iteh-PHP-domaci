819cd0c4920f9e5ba6281a5ba07e408a7c351cb1
Blazo Sokic <blazosokic@outlook.com>
2022-02-04 11:35:24 +0100

    poslednje izmjene - mjenjanje css-a

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <?php
 include "connection.php";
-session_start(); // Starting Session
-$error=''; // Variable To Store Error Message
+session_start(); 
+$error=''; 
 if (isset($_POST['submit'])) {
 if (empty($_POST['username']) || empty($_POST['password'])) {
 $error = "Username or Password is invalid";
 }
 else
 {
-// Define $username and $password
+
 $username=$_POST['username'];
 $password=$_POST['password'];
 
@@ -16,13 +16,13 @@ $username = stripslashes($username);
 $password = stripslashes($password);
 
 
-// SQL query to fetch information of registerd users and finds user match.
+
 $query = "select * from user where password='$password' AND username='$username'";
 $rezultat = $mysqli->query($query);
 
 if ($rezultat == 1) {
-$_SESSION['login_user']=$username; // Initializing Session
-header("location: index.php"); // Redirecting To Other Page
+$_SESSION['login_user']=$username; 
+header("location: index.php"); 
 } else {
 $error = "Username or Password is invalid";
 }
--- session.php
+++ session.php
@@ -1,10 +1,10 @@
 <?php
 include "connection.php";
 
-session_start();// Starting Session
-// Storing Session
+session_start();
+
 $user_check=$_SESSION['login_user'];
-// SQL Query To Fetch Complete Information Of User
+
 $sesql="select username from user where username='$user_check'";
 $rez= $mysqli->query($sesql);
 $red = $rez->fetch_object();
@@ -12,6 +12,6 @@ $red = $rez->fetch_object();
 $login_session =$red->username;
 if(!isset($login_session)){
 
-header('Location: indexLog.php'); // Redirecting To Home Page
+header('Location: indexLog.php'); 
 }
 ?>\
